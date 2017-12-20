9410f00df52c7ea3cfd47e5cc4fe5b7aa0d624f3
Evolution2301 <chmo1994@arcor.de>
2017-12-20 10:14:50 +0100

    checking in solution of Day1

--- AdventOfCode2017/Days/Day1.cs
+++ AdventOfCode2017/Days/Day1.cs
@@ -7,11 +7,36 @@ using System.Threading.Tasks;
 namespace AdventOfCode2017.Days {
     class Day1 : IDay {
         public string SolveP1(string input) {
-            throw new NotImplementedException();
+            int sum = 0;
+            for(int i=0; i < input.Length; i++) {
+                if(i+1 == input.Length) {
+                    if(input[i] == input[0]) {
+                        sum += int.Parse(""+input[i]);
+                    }
+                } else {
+                    if(input[i] == input[i+1]) {
+                        sum += int.Parse(""+input[i]);
+                    }
+                }
+            }
+            return $"{sum}";
         }
 
         public string SolveP2(string input) {
-            throw new NotImplementedException();
+            int sum = 0;
+            int step = input.Length / 2;
+            for (int i = 0; i < input.Length; i++) {
+                if (i + step >= input.Length) {
+                    if (input[i] == input[i- input.Length + step]) {
+                        sum += int.Parse("" + input[i]);
+                    }
+                } else {
+                    if (input[i] == input[i + step]) {
+                        sum += int.Parse("" + input[i]);
+                    }
+                }
+            }
+            return $"{sum}";
         }
     }
 }
